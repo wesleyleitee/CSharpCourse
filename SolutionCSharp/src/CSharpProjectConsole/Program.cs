c232b5006de41bec19b04c91d63d05519e20f241
Wesley Leite <leitewesleyleite@gmail.com>
2025-11-10 08:59:01 -0300

    Classe

--- SolutionCSharp/src/CSharpProjectConsole/Program.cs
+++ SolutionCSharp/src/CSharpProjectConsole/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Person p = new Person();
+p.Name = "Wesley";
+p.Age = 18;
+p.Present();
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age {  get; set; }
+
+    public void Present()
+    {
+        Console.WriteLine($"Hello, my name is {Name}, I am {Age} years old");
+    }
+}
